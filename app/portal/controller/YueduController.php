82dc8bf668e1b9719bb6bdb128f28bd44b70aef9
Yang Liu <yang.liu@mdpi.com>
2018-04-22 20:07:24 +0800

    add ydinfo

--- app/portal/controller/YueduController.php
+++ app/portal/controller/YueduController.php
@@ -14,6 +14,10 @@ use cmf\controller\HomeBaseController;
 use think\Db;
 use app\portal\model\PortalPostModel;
 use app\portal\service\ApiService;
+use app\portal\model\PortalCategoryModel;
+use app\portal\service\PostService;
+
+
 
 class YueduController extends HomeBaseController
 {
@@ -27,5 +31,57 @@ class YueduController extends HomeBaseController
         $this->assign('articles',$articles);
         //print_r($article);exit;
         return $this->fetch(':yuedu');
+    }
+
+    public function info()
+    {
+
+        $portalCategoryModel = new PortalCategoryModel();
+        $postService         = new PostService();
+
+        $articleId  = $this->request->param('id', 0, 'intval');
+        $categoryId = $this->request->param('cid', 0, 'intval');
+        $article    = $postService->publishedArticle($articleId, $categoryId);
+        if (empty($article)) {
+            abort(404, '文章不存在!');
+        }
+
+
+        $prevArticle = $postService->publishedPrevArticle($articleId, $categoryId);
+        $nextArticle = $postService->publishedNextArticle($articleId, $categoryId);
+
+        $tplName = 'article';
+
+        if (empty($categoryId)) {
+            $categories = $article['categories'];
+
+            if (count($categories) > 0) {
+                $this->assign('category', $categories[0]);
+            } else {
+                abort(404, '文章未指定分类!');
+            }
+
+        } else {
+            $category = $portalCategoryModel->where('id', $categoryId)->where('status', 1)->find();
+            if (empty($category)) {
+                abort(404, '文章不存在!');
+            }
+
+            $this->assign('category', $category);
+
+            $tplName = empty($category["one_tpl"]) ? $tplName : $category["one_tpl"];
+        }
+
+        Db::name('portal_post')->where(['id' => $articleId])->setInc('post_hits');
+
+
+        hook('portal_before_assign_article', $article);
+
+        $this->assign('article', $article);
+        $this->assign('prev_article', $prevArticle);
+        $this->assign('next_article', $nextArticle);
+        $this->assign('photos', $article['more']['photos']);
+        $tplName = empty($article['more']['template']) ? $tplName : $article['more']['template'];
+        return $this->fetch(":ydinfo");
     }
 }
