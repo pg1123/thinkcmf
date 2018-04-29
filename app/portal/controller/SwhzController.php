ab6e8c8995aad8bf0182b03c1a078e3cd8d99998
Yang Liu <yang.liu@mdpi.com>
2018-04-29 17:41:58 +0800

    swhz

---
+++ app/portal/controller/SwhzController.php
@@ -0,0 +1,29 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 老猫 <thinkcmf@126.com>
+// +----------------------------------------------------------------------
+namespace app\portal\controller;
+
+use cmf\controller\HomeBaseController;
+use think\Db;
+use app\portal\model\PortalPostModel;
+use app\portal\service\ApiService;
+use app\portal\model\PortalCategoryModel;
+use app\portal\service\PostService;
+
+
+
+class SwhzController extends HomeBaseController
+{
+    public function index()
+    {
+        return $this->fetch(':swhz');
+    }
+
+}
